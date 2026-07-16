af3f4ca714c9cf2867e8ee181b6233b0990bd7cd
hephaestushex <81438978+hephaestushex@users.noreply.github.com>
2026-07-15 20:21:53 -0700

    egegg

--- keystroke-firmware/src/main.cpp
+++ keystroke-firmware/src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define BUILTIN_LED 25
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(BUILTIN_LED, GPIO_OUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(BUILTIN_LED, HIGH);
+  delay(1000);
+  digitalWrite(BUILTIN_LED, LOW);
+  delay(1000);
 }
